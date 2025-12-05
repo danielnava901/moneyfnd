9cb2788fea0626bc6fa46b032c7d162c93243597
Daniel Nava Viveros <daniel.navaviveros@nutanix.com>
2025-12-05 12:41:47 -0600

    const env

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,7 @@
-//const URL_API = "https://moneybkd.vercel.app/api"
-const URL_API = "http://localhost:9992/api"
+const DEV = "development";
+const PROD = "production";
+const ENV = import.meta.env.VITE_ENV || DEV;
+const URL_API = ENV === PROD ? import.meta.env.VITE_API_BKD_URL : "http://localhost:9992/api";
 
 export {
     URL_API
