f8439ffdd9ea518cfe27c070707ed3d9f7a6150d
Daniel Nava Viveros <daniel.navaviveros@nutanix.com>
2026-01-09 10:48:15 -0600

    date range

--- app/components/Filters.tsx
+++ app/components/Filters.tsx
@@ -1,9 +1,23 @@
 import type {OptionItemType} from "~/models/models";
 import {useState} from "react";
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {DemoContainer} from "@mui/x-date-pickers/internals/demo";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import type {DateRange} from "@mui/x-date-pickers-pro";
+import dayjs, {type Dayjs} from "dayjs";
+
 
 const ButtonStyle = "px-2 py-1 border border-blue-400 text-xs font-medium rounded-full cursor-pointer"
 const ButtonActiveStyle = "border-blue-400 bg-blue-500 text-white cursor-pointer"
+const TODAY = new Date();
+const LAST_WEEK = new Date()
+LAST_WEEK.setDate(TODAY.getDate() - 7);
 
+console.log({
+    TODAY,
+    LAST_WEEK,
+})
 const items : OptionItemType[] = [
     { label: "Three days", value: -3 },
     { label: "Five days", value: -5 },
@@ -17,12 +31,15 @@ const Filters = ({onClickFilter} : {
 
     const [showModal, setShowModal] = useState(false);
     const [currentFilter, setCurrentFilter] = useState<OptionItemType | null>(items[2]);
+    const [rangeValue, setRangeValue] = useState<DateRange<Dayjs>>([
+        dayjs(LAST_WEEK.toString()),
+        dayjs(TODAY.toDateString())
+    ]);
 
     const diffDates = (fecha: Date|string) => {
-        const today = new Date();
         const d = new Date(fecha);
         const msPerDay: number = 1000 * 60 * 60 * 24;
-        return Math.round(((d.getTime() - today.getTime()) / msPerDay) + 1);
+        return Math.round(((d.getTime() - TODAY.getTime()) / msPerDay));
     }
 
     return  <div className="flex gap-2 w-full justify-end p-2">
@@ -56,23 +73,26 @@ const Filters = ({onClickFilter} : {
                 <h3 className="text-lg font-medium mb-4 ">Custom filters</h3>
                 <form onSubmit={(e) => {
                     e.preventDefault();
-                    const form = e.target as HTMLFormElement;
-                    const start = (form.elements.namedItem('start') as HTMLInputElement)?.value;
-                    const end = (form.elements.namedItem('end') as HTMLInputElement)?.value;
+                    const start = rangeValue[0]?.toISOString();
+                    const end = rangeValue[1]?.toISOString();
 
-                    const startDate = diffDates(start);
-                    const endDate = diffDates(end);
-                    if (start) {
+                    if (start && end) {
+                        const startDate = diffDates(start);
+                        const endDate = diffDates(end);
                         onClickFilter(startDate, endDate);
                     }
                     setShowModal(false);
                     setCurrentFilter({label: "custom", value: "custom"});
                 }}>
-                    <div className="flex gap-2 mb-4">
-                        <input name="start" type="date" className="border p-2 rounded w-1/2 " />
-                        <input name="end" type="date" className="border p-2 rounded w-1/2 " />
-                    </div>
-                    <div className="flex justify-end gap-2 ">
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DemoContainer components={['DateRangePicker']}>
+                            <DateRangePicker
+                                value={rangeValue}
+                                onChange={(newValue) => setRangeValue(newValue)}
+                            />
+                        </DemoContainer>
+                    </LocalizationProvider>
+                    <div className="flex justify-end gap-2 mt-4">
                         <button type="button" className={`${ButtonStyle} bg-gray-200`} onClick={() => {
                             setShowModal(false);
                             setCurrentFilter(items[2]);
